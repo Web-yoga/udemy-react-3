0fd78d0311934d84613498e983e06898d03db540
Web-yoga <hyehaeng@gmail.com>
2021-07-23 17:48:08 +0300

    97 lesson

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+	return (
+		<div class="loadingio-spinner-gear-13id3zp3ied8"><div class="ldio-93nzhwi3cok">
+		<div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+		</div></div>
+	)
+}
+
+export default Spinner;
+
+
